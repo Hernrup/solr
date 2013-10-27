4c48d84706db3edd0b0f3089914f71432a0b49c6
Mikael Hernrup <mikael@hernrup.se>
2013-10-27 09:22:41 +0100

    Push to github

---
+++ instance/Client/DebugConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebugConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Actions.addRecord(@"http://localhost:8983/solr/lime/", "classname", 001, "Some content");
+
+            byte[] b = System.IO.File.ReadAllBytes(@"C:\dev\solr\instance\Client\Client\Presentation.pdf");
+
+            Actions.addFile(@"http://localhost:8983/solr/lime/", "classname", 003, b.ToString());
+            Console.ReadLine();
+        }
+    }
+}
